82a33f1f26d113d7c43f38e15aa3a13d3090362b
Kim Nejudne <august.nejudne@gmail.com>
2017-06-20 09:56:42 +0800

    Alpha4
    Changed bg
    added animations
    added pictures tp portfolio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-$('.particles').particleground({
-    dotColor: '#293f72',
-    lineColor: '#182550',
-    density: 10000
+// $('.particles').particleground({
+//     dotColor: '#293f72',
+//     lineColor: '#182550',
+//     density: 10000
+// });
+// 
+
+AOS.init({
+  duration: 1200,
+})
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 200) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(400);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(400);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 400);
 });
