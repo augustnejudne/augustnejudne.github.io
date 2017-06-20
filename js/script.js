739001cc7c39aa4828f072a1c62fd5ab84cb2b38
Kim Nejudne <august.nejudne@gmail.com>
2017-06-20 12:09:56 +0800

    Alpha4.3.2
    Put the background in its own div with position fixed.

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ AOS.init({
 
 // ===== Scroll to Top ==== 
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 200) {        // If page is scrolled more than 50px
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(400);    // Fade in the arrow
     } else {
         $('#return-to-top').fadeOut(400);   // Else fade out the arrow
